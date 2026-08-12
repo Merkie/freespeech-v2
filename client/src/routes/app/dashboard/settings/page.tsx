2405296efac3eca4504ed2d9f3be83be2f4eba9b
Archer <archercalder@gmail.com>
2026-08-12 20:35:41 +0000

    Remove unused settings cards

--- client/src/routes/app/dashboard/settings/page.tsx
+++ client/src/routes/app/dashboard/settings/page.tsx
@@ -38,32 +38,6 @@ const SettingsPage: Component = () => {
 				</div>
 
 				<div class="flex flex-col gap-8 md:flex-row">
-					<a
-						href="/app/dashboard/settings/"
-						class="group pointer-events-none flex flex-1 items-center gap-4 rounded-xl border-2 border-zinc-300 p-4 opacity-50 grayscale transition-all select-none hover:border-orange-200 hover:bg-orange-50"
-					>
-						<div class="grid h-[70px] w-[70px] place-items-center rounded-lg bg-orange-100">
-							<i class="bi bi-people-fill text-[40px] text-orange-500"></i>
-						</div>
-						<span class="text-3xl text-zinc-800 transition-all group-hover:text-orange-500">Sharing</span>
-						<div class="flex-1"></div>
-						<i class="bi bi-arrow-right-short text-6xl text-zinc-500 transition-all group-hover:text-orange-500"></i>
-					</a>
-
-					<a
-						href="/app/dashboard/settings/"
-						class="group pointer-events-none flex flex-1 items-center gap-4 rounded-xl border-2 border-zinc-300 p-4 opacity-50 grayscale transition-all select-none hover:border-green-200 hover:bg-green-50"
-					>
-						<div class="grid h-[70px] w-[70px] place-items-center rounded-lg bg-green-100">
-							<i class="bi bi-bar-chart-fill text-[40px] text-green-500"></i>
-						</div>
-						<span class="text-3xl text-zinc-800 transition-all group-hover:text-green-500">Usage</span>
-						<div class="flex-1"></div>
-						<i class="bi bi-arrow-right-short text-6xl text-zinc-500 transition-all group-hover:text-green-500"></i>
-					</a>
-				</div>
-
-				<div class="flex flex-col gap-8 md:flex-row">
 					<A
 						href="/app/dashboard/settings/access-controls"
 						class="group flex flex-1 items-center gap-4 rounded-xl border-2 border-zinc-300 p-4 transition-all select-none hover:border-amber-200 hover:bg-amber-50"
